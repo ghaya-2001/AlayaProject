7bf4fc390ecddbdb9ef5b908060ec05cc83efa11
Ghaya Derbali <ghayaderbali20@gmail.com>
2023-02-20 14:57:40 +0100

    up

--- src/Components/Overview/Overview.jsx
+++ src/Components/Overview/Overview.jsx
@@ -13,20 +13,40 @@ function Overview() {
         <div className='box'>
         <div className='usercat'>
         <div className='user'>
-            <BsPeopleFill className='BsP'>users</BsPeopleFill>
+            <BsPeopleFill className='BsP'></BsPeopleFill>
+            <div className='num7'>
+            <div className='edit'>users</div>
+           
+            </div>
+            
         </div>
         <div className='vendors'>
             <AiOutlineInbox className='AiO' ></AiOutlineInbox>
+            <div className='num7'>
+            <div className='edit'>vendors</div>
+          
+            </div>
         </div>
         </div>
 
-        <div className='vendprod'>
+        <div className='catprod'>
         <div className='cat'>
+
              <BsUnion className='BsU' ></BsUnion>
+             <div className='num7'>
+             <div className='edit'>categories</div>
+          
+             </div>
+            
         
         </div>
         <div className='prod'>
-        <BsFileTextFill className='BsF'></BsFileTextFill>        </div>
+            <BsFileTextFill className='BsF'></BsFileTextFill> 
+            <div className='num7'>
+           <div className='edit'>product</div>
+      
+           </div>
+        </div>
         </div>
         </div>
     </div>
