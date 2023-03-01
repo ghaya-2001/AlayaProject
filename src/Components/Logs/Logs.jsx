424b4f6c7779640d0b1b39b2aff242dcd40b52f8
Ghaya Derbali <ghayaderbali20@gmail.com>
2023-03-01 17:15:39 +0100

    up

--- src/Components/Logs/Logs.jsx
+++ src/Components/Logs/Logs.jsx
@@ -7,13 +7,34 @@ import React from 'react'
 function Logs() {
     return (
         <div className='logs'>
-            <span className='title'>Logs</span>
-            <div className='row'>
-                <div className='item1'>Created</div>
-            </div>
-            <div className='hr'><div />
 
-                <div className='item2'>Deleted</div>
+            <div className='row'>
+                <div className='row0'>
+
+                    <span className='titlelog'>Logs</span>
+                    <div className='list'>21Dec2022</div>
+
+                </div>
+
+                <div className='row1'>
+                    <div className='item1'>Created</div>
+                </div>
+                <div className='row2'>
+                    <div className='item2'>Deleted</div>
+                </div>
+
+                <div className='row3'>
+                <div className='item3'>Deleted</div>
+
+                </div>
+               <div className='row4'>
+               <div className='item4'>Edited</div>
+
+               </div>
+            
+
+
+
 
 
 
